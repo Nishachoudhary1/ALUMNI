281e8e0c5a819fee9e476dc3e2d7b289431e4d62
Nisha Choudhary <nc161034@gmail.com>
2021-12-18 00:02:37 +0530

    new

--- script.js
+++ script.js
@@ -3,3 +3,32 @@ $(document).ready(function(){
         $('ul').toggleClass('show');
     });
 });
+
+/********************* for year in user_details page ********************/
+
+const yearSelect = document.getElementById("year");
+
+function populateYears(){
+    //Get the current year as a number
+    let year = new Date().getFullYear();
+    //make the previous 100 years be an option
+    for(let i = 0; i < 101; i++){
+        const option = document.createElement("option");
+        option.textContent = year-i;
+        yearSelect.appendChild(option);
+    }
+}
+populateYears();
+
+/******************* for extra option "other" in drop down menu *************/
+
+function toggleOther(){
+    var deg = document.getElementById('toToggle');
+    var strUser = deg.options[deg.selectedIndex].text;
+
+    if (strUser == 'Other'){
+        document.getElementById('extra-option').disabled = false;
+    }else{
+        document.getElementById('extra-option').disabled = true;
+    }
+}
